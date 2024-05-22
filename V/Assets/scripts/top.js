367806a07c29b6da42c408e91bb073846da3023c
LMNoire <149476454+LMNoire@users.noreply.github.com>
2024-05-22 14:47:24 +0200

    [Fix] Correctif suite rework

--- V/Assets/scripts/top.js
+++ V/Assets/scripts/top.js
@@ -1,18 +1,19 @@
-// Lorsque l'utilisateur fait défiler vers le bas de 20 pixels à partir du haut du document, affichez le bouton
 window.onscroll = function() {
     scrollFunction();
-  };
-  
-  function scrollFunction() {
+};
+
+function scrollFunction() {
+    var backToTopBtn = document.getElementById("backToTopBtn");
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("backToTopBtn").style.display = "block";
+        backToTopBtn.style.display = "block";
+        backToTopBtn.classList.add("hover"); // Ajoute la classe de survol
     } else {
-      document.getElementById("backToTopBtn").style.display = "none";
+        backToTopBtn.style.display = "none";
+        backToTopBtn.classList.remove("hover"); // Supprime la classe de survol
     }
-  }
-  
-  // Lorsque l'utilisateur clique sur le bouton, faites défiler vers le haut du document
-  function topFunction() {
+}
+
+function topFunction() {
     document.body.scrollTop = 0; // Pour Safari
     document.documentElement.scrollTop = 0; // Pour Chrome, Firefox, IE et Opera
-  }
+}
